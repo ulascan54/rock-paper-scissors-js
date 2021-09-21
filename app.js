b136cf28f8c1df446595f33d927b250c02888572
ulascan54 <ulascandemirbag@gmail.com>
2021-09-21 18:02:20 +0300

    and add more detail and finished

--- app.js
+++ app.js
@@ -7,11 +7,10 @@ const result_div=document.querySelector(".result")
 const rock_div=document.getElementById("r")
 const paper_div=document.getElementById("p")
 const scissors_div=document.getElementById("s")
-
+const choices=document.querySelectorAll('.choice')
 const comp_choice = document.querySelector(".comp-choice")
 const user_choice = document.querySelector(".user-choice")
 let comp_random=0
-
 
 const getComputerChoice= ()=>{
     const choices=['r','p','s']
@@ -57,6 +56,7 @@ let randomcomp= setInterval(() => {
         }, 200);
 
 const game = (userChoice) => {
+    removemain()
     clearInterval(randomcomp)
     if(userChoice=='r'){
         user_choice.children[0].classList.add('hidden')
@@ -107,52 +107,133 @@ const game = (userChoice) => {
     const computerChoice = getComputerChoice()
     if(userChoice=='p' && computerChoice=='p'){
         result_div.textContent='Your choices are the same. Draw'
+
+        paper_div.classList.add('gray-glow')
+        choices.forEach(e =>e.classList.add('choice-not'));
+        setTimeout(() => {
+                 choices.forEach(e =>e.classList.remove('choice-not'));
+             paper_div.classList.remove('gray-glow')
+             main()
+        }, 2000);
     }
     else if(userChoice=='p' && computerChoice=='r'){
         result_div.textContent='Paper covers the rock You win!'
         userScore_span.textContent=Number(userScore_span.textContent)+1
+
+        paper_div.classList.add('green-glow')
+        choices.forEach(e =>e.classList.add('choice-not'));
+        setTimeout(() => {
+                 choices.forEach(e =>e.classList.remove('choice-not'));
+             paper_div.classList.remove('green-glow')
+             main()
+        }, 2000);
+        
     }
     else if(userChoice=='p' && computerChoice=='s'){
         result_div.textContent='Scissors cuts paper You lost :('
         computerScore_span.textContent=Number(computerScore_span.textContent)+1
+
+        paper_div.classList.add('red-glow')
+        choices.forEach(e =>e.classList.add('choice-not'));
+        setTimeout(() => {
+                 choices.forEach(e =>e.classList.remove('choice-not'));
+             paper_div.classList.remove('red-glow')
+             main()
+        }, 2000);
     }
     else if(userChoice=='r' && computerChoice=='r'){
         result_div.textContent='Your choices are the same. Draw'
+
+        rock_div.classList.add('gray-glow')
+        choices.forEach(e =>e.classList.add('choice-not'));
+        setTimeout(() => {
+            rock_div.classList.remove('gray-glow')
+                 choices.forEach(e =>e.classList.remove('choice-not'));
+             main()
+        }, 2000);
     }
     else if(userChoice=='r' && computerChoice=='p'){
         result_div.textContent='Paper covers the rock You lost :('
         computerScore_span.textContent=Number(computerScore_span.textContent)+1
+
+        rock_div.classList.add('red-glow')
+        choices.forEach(e =>e.classList.add('choice-not'));
+        setTimeout(() => {
+            rock_div.classList.remove('red-glow')
+                choices.forEach(e =>e.classList.remove('choice-not'));
+            main()
+        }, 2000);
     }
     else if(userChoice=='r' && computerChoice=='s'){
         result_div.textContent='Stone breaks scissors You win!'
         userScore_span.textContent=Number(userScore_span.textContent)+1
+
+        rock_div.classList.add('green-glow')
+        choices.forEach(e =>e.classList.add('choice-not'));
+        setTimeout(() => {
+                 choices.forEach(e =>e.classList.remove('choice-not'));
+             rock_div.classList.remove('green-glow')
+             main()
+        }, 2000);
     }
     else if(userChoice=='s' && computerChoice=='s'){
         result_div.textContent='Your choices are the same. Draw'
+
+        scissors_div.classList.add('gray-glow')
+        choices.forEach(e =>e.classList.add('choice-not'));
+        setTimeout(() => {
+                 choices.forEach(e =>e.classList.remove('choice-not'));
+             scissors_div.classList.remove('gray-glow')
+             main()
+        }, 2000);
+        
     }
     else if(userChoice=='s' && computerChoice=='r'){
         result_div.textContent='Stone breaks scissors You lost :('
         computerScore_span.textContent=Number(computerScore_span.textContent)+1
+
+        scissors_div.classList.add('red-glow')
+        choices.forEach(e =>e.classList.add('choice-not'));
+        setTimeout(() => {
+                 choices.forEach(e =>e.classList.remove('choice-not'));
+             scissors_div.classList.remove('red-glow')
+             main()
+        }, 2000);
     }
     else if(userChoice=='s' && computerChoice=='p'){
         result_div.textContent='Scissors cuts paper You win!'
         userScore_span.textContent=Number(userScore_span.textContent)+1
+
+        scissors_div.classList.add('green-glow')
+        choices.forEach(e =>e.classList.add('choice-not'));
+        setTimeout(() => {
+            choices.forEach(e =>e.classList.remove('choice-not'));
+             scissors_div.classList.remove('green-glow')
+             main()
+        }, 2000);
     }
+}
+
+const  rockf= () => {
+    game('r')
+}
+const paperf = () => {
+    game('p')
+}
+
+const scissorsf = () => {
+    game('s')
 }
 
 
 const main = () => {
-    rock_div.addEventListener('click',()=>{
-        game('r')
-    })
-    
-    paper_div.addEventListener('click',()=>{
-        game('p')
-    })
-    
-    
-    scissors_div.addEventListener('click',()=>{
-        game('s')
-    })
+    rock_div.addEventListener('click',rockf)
+    paper_div.addEventListener('click',paperf)
+    scissors_div.addEventListener('click',scissorsf)
+}
+const removemain=()=>{
+    rock_div.removeEventListener('click',rockf)
+    paper_div.removeEventListener('click',paperf)
+    scissors_div.removeEventListener('click',scissorsf)
 }
 main()
